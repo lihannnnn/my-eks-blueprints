280aab306c5f079172f4e789c98c18c40b36ec96
Han Li <hanlih@amazon.com>
2022-09-03 14:50:30 +0000

    Bootstrapping ArgoCD

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -16,7 +16,35 @@ export default class PipelineConstruct extends Construct {
     .region(region)
     .addOns(new blueprints.ClusterAutoScalerAddOn) // Cluster Autoscaler addon goes here
     .teams(new TeamPlatform(account), new TeamApplication('burnham',account)); // HERE WE ADD THE TEAMS
-  
+ 
+ // HERE WE ADD THE ARGOCD APP OF APPS REPO INFORMATION
+    const repoUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git';
+
+    const bootstrapRepo : blueprints.ApplicationRepository = {
+        repoUrl,
+        targetRevision: 'workshop',
+    }
+
+  // HERE WE GENERATE THE ADDON CONFIGURATIONS
+    const devBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/dev'
+        },
+    });
+    const testBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/test'
+        },
+    });
+    const prodBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/prod'
+        },
+    });
+
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
       .owner("lihannnnn")
@@ -29,9 +57,9 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
-          { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
+          { id: "dev", stackBuilder: blueprint.clone('us-west-2').addOns(devBoostrapArgo)},
+          { id: "test", stackBuilder: blueprint.clone('us-east-2').addOns(testBoostrapArgo},
+          { id: "prod", stackBuilder: blueprint.clone('us-east-1').addOns(prodBoostrapArgo)}
         ]
       })
       .build(scope, id+'-stack', props);
